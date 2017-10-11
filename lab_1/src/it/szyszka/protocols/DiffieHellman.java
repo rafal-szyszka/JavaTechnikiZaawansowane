d9b3cacaa444aea398bc9aababece83342620302
Rafał Szyszka <rafal@szyszka.it>
2017-10-11 20:21:37 +0200

    implementing diffie hellman

---
+++ lab_1/src/it/szyszka/protocols/DiffieHellman.java
@@ -0,0 +1,15 @@
+package it.szyszka.protocols;
+
+import java.math.BigInteger;
+
+public class DiffieHellman {
+
+    // TODO: 11.10.17 read them from properties?!
+    public static final int PRIMAL = 23;
+    public static final int BASE = 5;
+
+    public static BigInteger generateA(Integer userChoice) {
+        BigInteger A = BigInteger.valueOf((long)Math.pow(BASE, userChoice));
+    }
+
+}
